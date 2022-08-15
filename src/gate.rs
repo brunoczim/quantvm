11515ce0bc9d133c472ab0d6552b8ea6c32eee33
brunoczim <brunoczim@gmail.com>
2022-08-15 00:05:54 -0300

    separate gate and memory

---
+++ src/gate.rs
@@ -0,0 +1,71 @@
+use crate::memory::Memory;
+use nalgebra::{DMatrix, SMatrix};
+use num::{complex::Complex64, Integer, Zero};
+
+pub trait Gate {
+    fn apply(&self, memory: &mut Memory, start: usize);
+}
+
+#[derive(Debug)]
+pub struct SGate<const N: usize> {
+    elements: SMatrix<Complex64, N, N>,
+}
+
+impl<const N: usize> SGate<N> {
+    pub fn from_matrix(elements: SMatrix<Complex64, N, N>) -> Self {
+        if N.is_odd() {
+            panic!("SGate must be have an even dimension, found {}", N);
+        }
+        Self { elements }
+    }
+}
+
+impl<const N: usize> Gate for SGate<N> {
+    fn apply(&self, memory: &mut Memory, start: usize) {
+        let mut qubits_matrix = memory.qubits_matrix_mut();
+        let mut slice = qubits_matrix.slice_mut((start, 0), (N, 2));
+        let mut output: SMatrix<Complex64, N, N> =
+            SMatrix::from_element(Complex64::zero());
+        self.elements.mul_to(&slice, &mut output);
+        slice.copy_from(&output);
+    }
+}
+
+#[derive(Debug)]
+pub struct DGate {
+    elements: DMatrix<Complex64>,
+}
+
+impl DGate {
+    pub fn from_matrix(elements: DMatrix<Complex64>) -> Self {
+        if !elements.is_square() {
+            panic!(
+                "Gate of dimensions {}x{} is not square matrix",
+                elements.shape().0,
+                elements.shape().1
+            );
+        }
+        if elements.shape().0.is_odd() {
+            panic!(
+                "DGate must be have an even dimension, found {}",
+                elements.shape().0
+            );
+        }
+        Self { elements }
+    }
+}
+
+impl Gate for DGate {
+    fn apply(&self, memory: &mut Memory, start: usize) {
+        let mut qubits_matrix = memory.qubits_matrix_mut();
+        let mut slice =
+            qubits_matrix.slice_mut((start, 0), (self.elements.shape().0, 2));
+        let mut output = DMatrix::from_element(
+            self.elements.shape().0,
+            self.elements.shape().1,
+            Complex64::zero(),
+        );
+        self.elements.mul_to(&slice, &mut output);
+        slice.copy_from(&output);
+    }
+}
---
+++ src/memory.rs
@@ -0,0 +1,42 @@
+use crate::gate::Gate;
+use nalgebra::{DMatrix, DMatrixSlice, DMatrixSliceMut};
+use num::{complex::Complex64, Zero};
+
+#[derive(Debug)]
+pub struct Memory {
+    qubits: DMatrix<Complex64>,
+}
+
+impl Memory {
+    pub fn from_matrix(matrix: DMatrix<Complex64>) -> Self {
+        if matrix.shape().1 != 1 {
+            panic!(
+                "Shape of qubit matrix must be a column vector, shape {}x{} \
+                 found",
+                matrix.shape().0,
+                matrix.shape().1
+            )
+        }
+
+        Self { qubits: matrix }
+    }
+
+    pub fn zeroed(size: usize) -> Self {
+        Self { qubits: DMatrix::from_element(size * 2, 1, Complex64::zero()) }
+    }
+
+    pub fn qubits_matrix(&self) -> DMatrixSlice<Complex64> {
+        self.qubits.slice((0, 0), self.qubits.shape())
+    }
+
+    pub fn qubits_matrix_mut(&mut self) -> DMatrixSliceMut<Complex64> {
+        self.qubits.slice_mut((0, 0), self.qubits.shape())
+    }
+
+    pub fn apply_gate<G>(&mut self, start: usize, gate: &G)
+    where
+        G: Gate,
+    {
+        gate.apply(self, start)
+    }
+}
